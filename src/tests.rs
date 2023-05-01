5e6be366fd427c83496c6089564bcfa6b0bd51b9
Dex <sheepareawesome777@gmail.com>
2023-05-01 15:33:50 -0400

    Split tests.rs code into `mod` blocks

--- src/tests.rs
+++ src/tests.rs
@@ -1,26 +1,38 @@
 use crate::*;
 use instructions::INSTR;
 
-#[test]
-fn do_nothing() -> Result<()> {
-	let res = execute_binary(
-		&[]
-	)?;
+#[cfg(test)]
+mod parsing {
+	use super::*;
 
-	assert_eq!(res, 0);
+	#[test]
+	fn do_nothing() -> Result<()> {
+		let res = execute_binary(
+			&[]
+		)?;
 
-	Ok(())
+		assert_eq!(res, 0);
+
+		Ok(())
+	}
+
+	#[test]
+	fn opcode_to_instr() -> Result<()> {
+		// Exit instruction
+		let exit_instr: INSTR = 0.try_into()?;
+		assert_eq!(exit_instr, INSTR::Exit);
+
+		// Invalid instruction
+		let invalid_instr: Result<INSTR, _> = 192_168.try_into();
+		assert!(invalid_instr.is_err());
+
+		Ok(())
+	}
 }
 
-#[test]
-fn opcode_to_instr() -> Result<()> {
-	// Exit instruction
-	let exit_instr: INSTR = 0.try_into()?;
-	assert_eq!(exit_instr, INSTR::Exit);
-
-	// Invalid instruction
-	let invalid_instr: Result<INSTR, _> = 192_168.try_into();
-	assert!(invalid_instr.is_err());
-
-	Ok(())
+#[cfg(test)]
+mod execution {
+	use super::*;
+	
+	//
 }
