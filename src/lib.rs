4bfb6fa9657dcf47d5177ab53ff4dfc14470bd22
Dex <sheepareawesome777@gmail.com>
2023-05-02 01:49:24 -0400

    interpret shit :P

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 * - Cherry 4/27/2023
 */
 
+use instructions::INSTR;
 use libdx::Result;
 
 mod instructions;
@@ -10,6 +11,31 @@ mod instructions;
 #[cfg(test)]
 mod tests;
 
-pub fn execute_binary(executable: &[u64]) -> Result<i64> {
-	Ok(0)
+pub fn execute_binary(program_bytes: &[u64]) -> Result<i64> {
+	let bytecode = program_bytes.iter();
+
+	let exit_code = 0;
+
+	for &opcode in bytecode {
+		let instruction: INSTR = (opcode as u32).try_into()?;
+
+		match instruction {
+			INSTR::Exit		=> todo!(),
+			INSTR::Push		=> todo!(),
+			INSTR::Pop		=> todo!(),
+			INSTR::Store	=> todo!(),
+			INSTR::Deref	=> todo!(),
+			INSTR::Incr		=> todo!(),
+			INSTR::Decr		=> todo!(),
+			INSTR::NOT		=> todo!(),
+			INSTR::AND		=> todo!(),
+			INSTR::OR		=> todo!(),
+			INSTR::Add		=> todo!(),
+			INSTR::Sub		=> todo!(),
+			INSTR::Mul		=> todo!(),
+			INSTR::Div		=> todo!(),
+		}
+	}
+
+	Ok(exit_code)
 }
